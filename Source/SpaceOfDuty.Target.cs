965026a00ac9a285a3ea7a20893a7142aa2698e6
fabiolamin <fabio_lamin@hotmail.com>
2026-06-30 19:27:55 -0300

    Updates project's version from 5.6 to 5.8

--- Source/SpaceOfDuty.Target.cs
+++ Source/SpaceOfDuty.Target.cs
@@ -8,8 +8,8 @@ public class SpaceOfDutyTarget : TargetRules
 	public SpaceOfDutyTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("SpaceOfDuty");
 	}
 }
